8a463e79ad15ee7342ea2068fb7e009988529260
armagan ozdemir <armaganozdemir@armagans-macbook-pro.local>
2019-01-12 22:18:18 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by armagan ozdemir on 12.01.2019.
+//  Copyright © 2019 armagan ozdemir. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title:String=""
+    var done:Bool=false
+}
