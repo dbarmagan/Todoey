64277b570cdb1a4be69f392ac76601bea8a0e0e5
armagan ozdemir <armaganozdemir@armagans-macbook-pro.local>
2019-01-22 22:53:24 +0300

    Persisting data using Realm

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by armagan ozdemir on 20.01.2019.
+//  Copyright © 2019 armagan ozdemir. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name:String=""
+    let items=List<Item>()
+}
